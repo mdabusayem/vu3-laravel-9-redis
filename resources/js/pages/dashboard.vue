b0485c18a078dc299126021461c475cd5b96598f
sayem <abusayem759@gmail.com>
2023-02-14 03:45:59 +0600

    running

---
+++ resources/js/pages/dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <h2>Dashboard : {{$store.getters.getToken}} </h2>
+        <button type="button" class="btn btn-dark mt-2" @click="logout">Logout</button>
+    </div>
+</template>
+<script>
+    import { useRouter } from "vue-router"
+    import { useStore } from 'vuex'
+    export default{
+     
+        setup(){
+            const router = useRouter();
+            const store = useStore();
+
+            function logout(){
+                store.dispatch('removeToken');
+                router.push({name:'Home'})
+            }
+            return {
+                logout
+            }
+        },
+        created() {
+            //console.log(auth()->user()->name);
+        }
+    }
+</script>
